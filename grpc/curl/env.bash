3e50bda09495a1c46b5365acfd738650b71ddf41
森崇 <kanetugu2015@gmail.com>
2022-12-18 09:36:23 +0900

    add curl

---
+++ grpc/curl/env.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+export GRPC_SERVER_ADDR=localhost
+export GRPC_SERVER_PORT=50051
+export GRPC_SPEC_PATH=./grpc/hakoniwa_core.proto
---
+++ grpc/curl/register.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+source grpc/curl/env.bash
+
+if [ $# -ne 1 ]
+then
+    echo "Usage: $0 <asset-name>"
+    exit 1
+fi
+ASSET_NAME=${1}
+
+grpcurl -proto \
+    ${GRPC_SPEC_PATH} \
+    -d "{\"name\": \"${ASSET_NAME}\" }" \
+    -plaintext \
+    ${GRPC_SERVER_ADDR}:${GRPC_SERVER_PORT} \
+    hakoniwa.CoreService.Register
